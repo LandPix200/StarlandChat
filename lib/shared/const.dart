03362ec06c54df1ed03691e4c1e46e60d35e9de4
Landry Simo <86841267+LandPix200@users.noreply.github.com>
2023-07-01 11:47:58 +0100

    first commit

---
+++ lib/shared/const.dart
@@ -0,0 +1,4 @@
+const imagePath = 'lib/resources/assets/images';
+const defaultWelcomeGreet =
+    "Salut, Je suis Starland un assistant d'IA \n Comment puis-je vous aider aujourd'hui?";
+const lottieBasePath = 'lib/resources/assets/lottie';
